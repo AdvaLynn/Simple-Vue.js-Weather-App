55f50c8fa372a5f0867c10426b713676abd24654
Adva Lynn Masliah <advalynnmasliah@Advas-MacBook-Pro.local>
2018-10-28 10:49:23 -0400

    fix bug of unfound cities

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-var weatherApp = new Vue({
+new Vue({
   el: '#app',
   data: {
     currentTemp: '',
@@ -11,32 +11,22 @@ var weatherApp = new Vue({
     wind: '',
     overcast: '',
     city: 'Waterloo',
-    alt: "Toronto"
+    alt: "Toronto",
+    city_country: "",
+    alt_country: "",
+    new_city: null,
+    new_country: null
 
   },
   methods: {
-    switchCity() {
-      var temp = this.city;
-      this.city = this.alt;
-      this.alt = temp;
-      if (this.city == "Toronto") {
-        document.body.style.backgroundImage = "url('../toronto.jpg')";
-      }
-      else if (this.city == "Waterloo") {
-        document.body.style.backgroundImage = "url('../waterloo.jpg')";
-      }
-      else {
-        document.body.style.backgroundImage = "url('../city.jpg')";
-      }
-    },
     getWeather() {
       //let url = "http://api.openweathermap.org/data/2.5/weather?q=toronto&units=metric&APPID=be979721f16e8857ea19be1e4316b84b";
       axios
         .get("http://api.openweathermap.org/data/2.5/weather", {
           params: {
-            q: this.city,
+            q: (this.new_city) ? this.new_city + this.new_country : this.city + this.city_country,
             units: "metric",
-            APPID: [Weather-API]
+            APPID: [WEATHER-API]
           }
         })
         .then(response => {
@@ -49,20 +39,52 @@ var weatherApp = new Vue({
           this.overcast = response.data.weather[0].description;
           this.sunrise = new Date(response.data.sys.sunrise*1000).toLocaleTimeString("en-GB");
           this.sunset = new Date(response.data.sys.sunset*1000).toLocaleTimeString("en-GB");
+          if (weatherApp.new_city) {
+            window.weatherApp.switchCity();
+          };
       })
       .catch(error => {
+        alert("Sorry could not find " + this.new_city)
+        this.new_city = null;
+        this.new_country = null;
+
         console.log(error);
       });
     },
+    switchCity() {
+      if (this.new_city) {
+        this.alt = this.city;
+        this.alt_country = this.city_country;
+        this.city = this.new_city;
+        this.city_country = this.new_country;
+        this.new_city = null;
+        this.new_country = null;
+      }
+      else {
+        var temp = this.city;
+        this.city = this.alt;
+        this.alt = temp;
+        temp = this.city_country;
+        this.city_country = this.alt_country;
+        this.alt_country = temp;
+      }
+      if (this.city == "Toronto") {
+        document.body.style.backgroundImage = "url('../toronto.jpg')";
+      }
+      else if (this.city == "Waterloo") {
+        document.body.style.backgroundImage = "url('../waterloo.jpg')";
+      }
+      else {
+        document.body.style.backgroundImage = "url('../city.jpg')";
+      }
+    }
   },
   beforeMount() {
     this.getWeather();
+    this.timer = setInterval(this.getWeather(), 30000)
     window.weatherApp = this;
   },
   beforeUpdate() {
     this.getWeather();
-  },
-  showAddCity() {
-    document.getElementById("pac-input").style.display = "block";
-  },
+  }
 });
