a179cd759aa02b8c9ba39a0d05fb7329adaf097d
Adva Lynn Masliah <advalynnmasliah@Advas-MacBook-Pro.local>
2018-10-28 01:06:18 -0400

    basic vue weather app to switch between two cities

--- src/main.js
+++ src/main.js
@@ -1,8 +1,68 @@
-import Vue from 'vue'
-import App from './App.vue'
+var weatherApp = new Vue({
+  el: '#app',
+  data: {
+    currentTemp: '',
+    minTemp: '',
+    maxTemp:'',
+    sunrise: '',
+    sunset: '',
+    pressure: '',
+    humidity: '',
+    wind: '',
+    overcast: '',
+    city: 'Waterloo',
+    alt: "Toronto"
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  },
+  methods: {
+    switchCity() {
+      var temp = this.city;
+      this.city = this.alt;
+      this.alt = temp;
+      if (this.city == "Toronto") {
+        document.body.style.backgroundImage = "url('../toronto.jpg')";
+      }
+      else if (this.city == "Waterloo") {
+        document.body.style.backgroundImage = "url('../waterloo.jpg')";
+      }
+      else {
+        document.body.style.backgroundImage = "url('../city.jpg')";
+      }
+    },
+    getWeather() {
+      //let url = "http://api.openweathermap.org/data/2.5/weather?q=toronto&units=metric&APPID=be979721f16e8857ea19be1e4316b84b";
+      axios
+        .get("http://api.openweathermap.org/data/2.5/weather", {
+          params: {
+            q: this.city,
+            units: "metric",
+            APPID: [Weather-API]
+          }
+        })
+        .then(response => {
+          this.currentTemp = response.data.main.temp;
+          this.minTemp = response.data.main.temp_min;
+          this.maxTemp = response.data.main.temp_max;
+          this.pressure = response.data.main.pressure;
+          this.humidity = response.data.main.humidity + '%';
+          this.wind = response.data.wind.speed + 'm/s';
+          this.overcast = response.data.weather[0].description;
+          this.sunrise = new Date(response.data.sys.sunrise*1000).toLocaleTimeString("en-GB");
+          this.sunset = new Date(response.data.sys.sunset*1000).toLocaleTimeString("en-GB");
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    },
+  },
+  beforeMount() {
+    this.getWeather();
+    window.weatherApp = this;
+  },
+  beforeUpdate() {
+    this.getWeather();
+  },
+  showAddCity() {
+    document.getElementById("pac-input").style.display = "block";
+  },
+});
